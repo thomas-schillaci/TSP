5c4bfbb7f947dcc69da5f6271600d5e14ffc1a7c
leovu <leovu@172.17.3.139>
2018-10-24 09:44:27 +0200

    LocalSearch version finale

--- src/tsp/deliverable/LocalSearchHeuristic.java
+++ src/tsp/deliverable/LocalSearchHeuristic.java
@@ -6,6 +6,9 @@ import tsp.Instance;
 import tsp.Solution;
 import tsp.heuristic.AHeuristic;
 
+/*
+ * Variable neighborhood search method
+ */
 public class LocalSearchHeuristic extends AHeuristic {
 	private Solution bestEver;
 	private Solution currentBest;
@@ -13,6 +16,7 @@ public class LocalSearchHeuristic extends AHeuristic {
 	private LSA_2optNeighborhood neighbors2;
 	private LSA_ShiftNeighborhood neighbors3;
 	private int methodChange;
+	private int stop;
 
 	public LocalSearchHeuristic(Instance instance, String name) throws Exception {
 		super(instance, name);
@@ -21,6 +25,7 @@ public class LocalSearchHeuristic extends AHeuristic {
 		neighbors2=new LSA_2optNeighborhood(instance, "neighbors");
 		neighbors3=new LSA_ShiftNeighborhood(instance, "neighbors");
 		methodChange = 0;
+		stop = 0;
 		// TODO Auto-generated constructor stub
 	}
 
@@ -35,30 +40,36 @@ public class LocalSearchHeuristic extends AHeuristic {
 	public void setCurrentEver(Solution currentBest) {
 		this.currentBest = currentBest;
 	}
+	
+	public boolean isStopped() {
+		return stop>=3;
+	}
 
 	@Override
 	public void solve() throws Exception {
 		// TODO Auto-generated method stub
 		switch (methodChange%3) {
-		case 1: 
-			List<Solution> neighborhood1 = neighbors1.getNeighborhood(bestEver);
-			for(Solution s : neighborhood1) { if(s.evaluate()<=bestEver.evaluate()) {currentBest=s;}}
-			if(currentBest.evaluate()<bestEver.evaluate()) {bestEver=currentBest;System.out.println(bestEver.evaluate()+" : 1");}
-			else {methodChange++;}
-			break;
-		
+		//The program starts with the 2-opt local search as it is fast and efficient
 		case 0:
 			List<Solution> neighborhood2 = neighbors2.getNeighborhood(bestEver); 
 			for(Solution s : neighborhood2) { if(s.evaluate()<=bestEver.evaluate()) {currentBest=s;}}
-			if(currentBest.evaluate()<bestEver.evaluate()) {bestEver=currentBest;System.out.println(bestEver.evaluate()+" : 2");}
-			else {methodChange++;}
+			if(currentBest.evaluate()<bestEver.evaluate()) {bestEver=currentBest;stop=0;}
+			else {methodChange++;stop++;}
 			break;
-			
+		//Trying to swap the last city index proves to be quite useful as the 2-opt method usually fail to
+		// find a correct last city choice.
+		case 1: 
+			List<Solution> neighborhood1 = neighbors1.getNeighborhood(bestEver);
+			for(Solution s : neighborhood1) { if(s.evaluate()<=bestEver.evaluate()) {currentBest=s;}}
+			if(currentBest.evaluate()<bestEver.evaluate()) {bestEver=currentBest;stop=0;}
+			else {methodChange++;stop++;}
+			break;
+		//Shifting parts of the solutions sometimes helps to escape a local minimum when the instances are big.
 		case 2:
 			List<Solution> neighborhood3 = neighbors3.getNeighborhood(bestEver); 
 			for(Solution s : neighborhood3) { if(s.evaluate()<=bestEver.evaluate()) {currentBest=s;}}
-			if(currentBest.evaluate()<bestEver.evaluate()) {bestEver=currentBest;System.out.println(bestEver.evaluate()+" : 3");}
-			else {methodChange++;}
+			if(currentBest.evaluate()<bestEver.evaluate()) {bestEver=currentBest;stop=0;}
+			else {methodChange++;stop++;}
 			break;
 		}
 	}
