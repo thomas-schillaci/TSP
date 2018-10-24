7f926d6a8977b8f195f75499c47a5f98d49e6239
Charles <Charles@Charles>
2018-10-24 18:26:10 +0200

    import heuristics

--- src/tsp/TSPSolver.java
+++ src/tsp/TSPSolver.java
@@ -1,6 +1,9 @@
 package tsp;
 
 import tsp.deliverable.AntHeuristic;
+import tsp.deliverable.BestInsertionHeuristic;
+import tsp.deliverable.BestInsertionOfNextCityHeuristic;
+import tsp.deliverable.NearestNeighborHeuristic;
 import tsp.heuristic.AHeuristic;
 
 /**
@@ -74,16 +77,18 @@ public class TSPSolver {
      * @throws Exception may return some error, in particular if some vertices index are wrong.
      */
     public void solve() throws Exception {
-        long startTime = System.currentTimeMillis();
-
-
-        AHeuristic heuristic = new AntHeuristic(m_instance);
-        while (System.currentTimeMillis() - startTime < m_timeLimit * 1000) {
-            heuristic.solve();
-            System.out.println(((AntHeuristic) heuristic).getLastObjectiveValue());
-        }
-
-        m_solution = heuristic.getSolution();
+//        long startTime = System.currentTimeMillis();
+//
+//
+//        AHeuristic heuristic = new AntHeuristic(m_instance);
+//        while (System.currentTimeMillis() - startTime < m_timeLimit * 1000) {
+//            heuristic.solve();
+//            System.out.println(((AntHeuristic) heuristic).getLastObjectiveValue());
+//        }
+//
+//        m_solution = heuristic.getSolution();
+    	AHeuristic h = new BestInsertionHeuristic(m_instance);
+    	h.solve();
     }
 
     // -----------------------------
